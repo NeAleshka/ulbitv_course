884f6dee6da9230dee490dfbb612cb1db3a3ce43
Ne_Alesha <alexeybudai@gmail.com>
2025-07-28 01:23:01 +0300

    добавление context для роута

--- src/routes/about.ts
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import AboutPage from "../widgets/About";
-
-export const Route = createFileRoute("/about")({
-  component: AboutPage,
-});
